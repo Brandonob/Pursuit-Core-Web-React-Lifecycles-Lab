3f3a30f56eb74b2a1868ec908c432cca8518e8fa
rafidhossain1995 <rafidhossain@pursuit.org>
2020-02-23 13:03:29 -0500

    save

--- lifecycle-lab/src/Component/Form.js
+++ lifecycle-lab/src/Component/Form.js
@@ -2,17 +2,31 @@ import React, {Component} from "react"
 
 class Form extends Component{
     state = {
-        list: " "
-    }
-
-    handleListInput = (e)=>{
-        e.preventDefault()
-       this.setState({
-           list: e.target.children[0].value
-        })
-        
+        list: [],
+        // handleChange: this.handleListInput
     }
     
+
+    handleListInput = (e)=>{
+        
+    e.preventDefault()
+    this.setState({
+        list:e.target.children[0].value
+        
+    })
+    debugger
+    }
+    // list: this.state.list.concat(e.target.children[0].value)
+
+    listOfItems = () =>{
+        let value = (this.state.list)
+        return(
+            <li key={value.toString()}>
+                {value}
+            </li>
+        )
+    }
+
     
     
     render(){
@@ -22,7 +36,11 @@ class Form extends Component{
             <form className = "form" onSubmit = {this.handleListInput}>
             <input type="text" placeholder="Enter a todo"/>
 
-
+            <ul>
+            <this.listOfItems />
+            </ul>
+                
+            
 
             </form>
             </div>
